37079674afcde35c1353273f9dc49061c8d80d89
Gleibson Vinicius Florêncio Nogueira <168869495+Gleibsonvinicius@users.noreply.github.com>
2025-03-26 19:40:43 -0300

    Update script.js

--- Estatistica de Partidas/script.js
+++ Estatistica de Partidas/script.js
@@ -1,7 +1,7 @@
 document.getElementById('form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Coletar dados do formulário
+    
     const jogos = parseInt(document.getElementById('jogos').value);
     const vitorias = parseInt(document.getElementById('vitorias').value);
     const empates = parseInt(document.getElementById('empates').value);
@@ -9,21 +9,21 @@ document.getElementById('form').addEventListener('submit', function(event) {
     const golsFeitos = parseInt(document.getElementById('golsFeitos').value);
     const golsSofridos = parseInt(document.getElementById('golsSofridos').value);
 
-    // Calcular a diferença de gols
+   
     const diferencaGols = golsFeitos - golsSofridos;
 
-    // Calcular a porcentagem de vitórias
+   
     const porcentagemVitorias = jogos > 0 ? (vitorias / jogos) * 100 : 0;
 
-    // Exibir os resultados
+    
     const resultadoDiv = document.getElementById('resultado');
     resultadoDiv.innerHTML = `
-        <h2>Resultados:</h2>
-        <p>Número total de jogos : ${jogos}</p>
-        <p>Número de vitórias: ${vitorias}</p>
-        <p>Número de empates: ${empates}</p>
-        <p>Número de derrotas: ${derrotas}</p>
-        <p>Diferença de gols : ${diferencaGols}</p>
+        <h2>Resultado:</h2>
+        <p> Jogos : ${jogos}</p>
+        <p> Vitórias: ${vitorias}</p>
+        <p> Empates: ${empates}</p>
+        <p> Derrotas: ${derrotas}</p>
+        <p> SG : ${diferencaGols}</p>
         <p>Porcentagem de vitórias: ${porcentagemVitorias.toFixed(2)}%</p>
     `;
 });
